9c48a110d4a10b7cbebbbea75325f2f26e7e6b1f
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-03 15:00:21 +0300

    lesson4

---
+++ .NET-Core-Yeni-Baslayanlar/ExceptionHandling/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("bir sayı giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("girmis oldugunuz sayi : " + sayi);
+            }
+            catch (Exception ex) 
+            { 
+                Console.Write(ex.Message.ToString());
+            }
+            // finally { Console.WriteLine("işlem tamamlandı"); }
+            
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("test");
+            }
+            catch (ArgumentException ex) 
+            {
+                Console.Write("veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            
+        }
+    }
+}
