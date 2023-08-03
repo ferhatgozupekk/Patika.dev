756b4167ce29853eb26e77405960aac3d03bea63
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-03 15:11:24 +0300

    lesson5

---
+++ .NET-Core-Yeni-Baslayanlar/İfElse/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace İfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11) { Console.WriteLine("günaydın."); }
+            else if (time < 18) { Console.WriteLine("iyi günler"); }
+            else { Console.WriteLine("iyi geceler"); }
+
+            string sonuc = time <= 18 ? "iyi günler" : "iyi geceler "; // ? = ise : = değilse anlamına gelir.
+            Console.WriteLine(sonuc);   
+        }
+    }
+}
