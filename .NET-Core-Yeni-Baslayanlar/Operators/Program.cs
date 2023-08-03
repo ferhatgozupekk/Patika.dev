93bcaaba8e08f1d512c47565910b2576115b664f
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-03 14:06:23 +0300

    lesson2

---
+++ .NET-Core-Yeni-Baslayanlar/Operators/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // atama ve işlemli atama
+            int x = 3;
+            int y = 4;  
+            int z = x + y ;
+            z *= x;
+            z /= y;
+
+            // mantıksal oparetorler
+            // || %% !
+
+            bool isTrue = false;    
+            bool isFull = false;   
+
+            if ( isTrue & isFull ) {
+                Console.WriteLine("hello");
+            }
+
+            // ilişkisel oparetorler 
+            // < , > , >= , <= , == , != 
+
+            int a = 1;
+            int b = 2;
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+
+            // % mod alır
+
+            int sonuc2 = 20 % 3;
+            Console.WriteLine(sonuc2);  
+
+
+
+
+
+
+        }
+    }
+}
