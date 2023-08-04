ed77dc56464a941d47476d00c18ce87cc7abebaa
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-04 16:32:04 +0300

    lesson20

---
+++ .NET-Core-Yeni-Baslayanlar/Constructor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calisan calisan1 = new Calisan("ferhat", "gözüpek", 1234 , "yönetim");
+            calisan1.CalisanBilgileri();
+
+            Calisan calisan = new Calisan();
+            calisan.Ad = "Ayse";
+            calisan.Soyad = "gozupek";
+            calisan.No = 12837123;
+            calisan.Departman = "yönetim";
+
+            calisan.CalisanBilgileri();
+            Console.ReadKey();  
+        }
+    }
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public Calisan (string ad, string soyad, int no, string departman)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            No = no;
+            Departman = departman;
+        }
+        public Calisan() { }
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("calisan adi : {0} ", Ad);
+            Console.WriteLine("calisan soyadı : {0} ", Soyad);
+            Console.WriteLine("calisan no : {0} ", No);
+            Console.WriteLine("calisan departmanı : {0} ", Departman);
+        }
+
+    }
+}
