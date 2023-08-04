1d24e6a7da0edac247deecd3c45d7939bc840354
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-04 15:21:34 +0300

    lesson18

---
+++ .NET-Core-Yeni-Baslayanlar/Dictionarys/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionarys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> kullanıcılar =  new Dictionary<int,string>();
+            kullanıcılar.Add(10, "ferhat gözüpek");
+            kullanıcılar.Add(12, "selin gözüpek");
+
+            // dizinin elemanlarına erişim
+            Console.WriteLine(kullanıcılar[12]);
+            foreach (var item in kullanıcılar)
+            {
+                Console.Write(item);
+            }
+             
+
+            //count
+            Console.WriteLine(kullanıcılar.Count);
+
+            //contains
+            Console.WriteLine(kullanıcılar.ContainsKey(12));
+            Console.WriteLine(kullanıcılar.ContainsValue("ferhat gözüpek"));
+
+
+            //remove
+            kullanıcılar.Remove(12);
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            //keys
+            foreach (var item in kullanıcılar.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            //values
+
+            foreach (var item in kullanıcılar.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
