72e438fc223a977645e169969e30b189f7a92f44
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-04 12:58:31 +0300

    lesson14

---
+++ .NET-Core-Yeni-Baslayanlar/Odev1_AlgorithmQuestions/StringMethods/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace StringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken1 = "merhaba benim adım ferhat gozupek";
+            string degisken2 = "sanada merhaba";
+
+            //length
+            Console.WriteLine(degisken1.Length);
+
+            // toupper tolower
+            Console.WriteLine(degisken1.ToUpper());    
+            Console.WriteLine(degisken1.ToLower());
+
+            //concat
+            Console.WriteLine(String.Concat(degisken1, "Merhaba"));
+            Console.WriteLine(String.Compare(degisken1, degisken2, true));
+            Console.WriteLine(String.Compare(degisken1, degisken2, false));
+
+            //contains 
+            Console.WriteLine(degisken1.Contains(degisken2));
+            Console.WriteLine(degisken1.EndsWith("gozupek"));
+
+            //indexof
+            Console.WriteLine   (degisken2.IndexOf("ana"));
+
+            // insert
+            Console.WriteLine(degisken2.Insert(0,"Merhaba"));
+
+            // padleft , padright
+
+            Console.WriteLine(degisken1 + degisken2.PadLeft(30));
+            Console.WriteLine(degisken1 + degisken2.PadLeft(30,'*'));
+
+            //Remove
+
+            Console.WriteLine(degisken1.Remove(10));
+            Console.WriteLine(degisken1.Remove(5,3));
+
+            //replace
+
+            Console.WriteLine(degisken1.Replace("sanada", "sanadaaaaaaa"));
+
+            //split
+
+            Console.WriteLine(degisken1.Split(' ')[1]);
+
+            //substring
+
+            Console.WriteLine(degisken1.Substring(4));
+            Console.WriteLine(degisken1.Substring(4,6));
+        }
+    }
+}
