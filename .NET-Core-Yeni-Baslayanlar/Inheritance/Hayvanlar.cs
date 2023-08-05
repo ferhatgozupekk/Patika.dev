af8d0b369338d7fe9189d4b889cd5c7583561ef8
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-05 08:47:48 +0300

    lesson23

---
+++ .NET-Core-Yeni-Baslayanlar/Inheritance/Hayvanlar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Hayvanlar :Bitkiler
+    {
+        public void Adaptasyon()
+        {
+
+            Console.WriteLine("hayvanlar adaptan kurabilir");
+
+        }
+    }
+    public class sürüngenler:Hayvanlar
+    {
+
+        public void SurunerekHareketEtmek()
+        {
+            Console.WriteLine("kuşlar uçar");
+        }
+
+    }
+    public class Kuslar:Hayvanlar
+    {
+        public void ucmak()
+        {
+            Console.WriteLine("kuslar ucar");
+        }
+    }
+
+}
---
+++ .NET-Core-Yeni-Baslayanlar/Inheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            tohumluBitkiler tohumluBitki = new tohumluBitkiler();
+            tohumluBitki.Beslenme();
+            tohumluBitki.FotosentezYapmak();
+            tohumluBitki.Solunum();
+            tohumluBitki.TohumlaCogalma();
+            tohumluBitki.Bosaltim();
+
+            Kuslar kuslar = new Kuslar();
+            kuslar.ucmak();
+
+        }
+    }
+}
