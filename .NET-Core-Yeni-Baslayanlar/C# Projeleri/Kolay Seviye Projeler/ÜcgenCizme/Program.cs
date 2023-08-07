fa0aa76c3dcd1311f2a7c4551a094f0ae1361344
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-07 16:15:54 +0300

    .

---
+++ .NET-Core-Yeni-Baslayanlar/C# Projeleri/Kolay Seviye Projeler/ÜcgenCizme/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÜcgenCizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ücgenin Boyutunu giriniz");
+            int secim = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= secim; i++)
+            { 
+                for ( int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
