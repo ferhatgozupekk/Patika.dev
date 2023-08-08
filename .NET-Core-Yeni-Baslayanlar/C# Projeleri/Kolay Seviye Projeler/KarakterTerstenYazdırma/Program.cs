cf7b082426b7f1ed137df29ab7148cec74b51fbd
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-08 15:52:18 +0300

    ekrana girilen cümleyi tersten yazdıran algoritma ödevi

---
+++ .NET-Core-Yeni-Baslayanlar/C# Projeleri/Kolay Seviye Projeler/KarakterTerstenYazdırma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KarakterTerstenYazdırma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cümle giriniz:");
+            string cumle = Console.ReadLine().TrimEnd().TrimStart();
+            string sonuc = string.Empty;
+
+            for (int i = cumle.Length - 1; i >= 0; i--)
+            {
+                sonuc += cumle[i];
+            }
+            Console.WriteLine("Cümlenin Ters Cevirilmis Hali: " + sonuc);
+            Console.ReadKey();
+
+        }
+    }
+}
