29cf05acc21ea06b4bdb52105296d667917e4f76
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-08 16:25:03 +0300

    ödev
    integer ikililerini işleme sokan algoritma ödevi

---
+++ .NET-Core-Yeni-Baslayanlar/C# Projeleri/Orta Seviye Projeler/Algoritma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.WebSockets;
+
+namespace Algoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var strings = input.Split(' ');
+            String str = string.Empty;
+            for (int i = 0; i < strings.Length; i += 2)
+            {
+
+                if (strings[i] == strings[i + 1])
+                {
+                    str += Math.Pow((Convert.ToInt32(strings[i]) + Convert.ToInt32(strings[i + 1])), 2) + " ";
+                }
+                else
+                {
+                    str += Convert.ToInt32(strings[i]) + Convert.ToInt32(strings[i + 1]) + " ";
+                }
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();  
+        }
+    }
+}
