96df1b54a9d0f5c10d676938be11d0dbb87373f5
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-08 17:22:57 +0300

    ödev

---
+++ .NET-Core-Yeni-Baslayanlar/C# Projeleri/Kolay Seviye Projeler/Ortalama Hesaplama/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ortalama_Hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Derinlik giriniz:");
+            int depth = int.Parse(Console.ReadLine());
+            int[] fibb = new int[depth];
+
+            fibb[0] = 1;
+
+            if( depth > 1 )
+            {
+                fibb[1] = 1;    
+            }
+
+            for (int i = 2; i < depth; i++)
+            {
+                fibb[i] = fibb[i - 1] + fibb[i - 2];
+            }
+
+            double sum = 0;
+            for (int i = 0; i < depth; i++)
+            {
+                sum += fibb[i];
+            }
+            foreach (var item in fibb)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine($"fibonacci serisi ortalaması: {sum / depth}" ); 
+        }
+    }
+}
