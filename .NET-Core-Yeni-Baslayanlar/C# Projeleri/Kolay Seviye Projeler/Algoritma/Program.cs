be6c5984ac65657a18a2e6bc5c7c9dc7d5b30f05
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-08 19:39:49 +0300

    algoritma ödevi

---
+++ .NET-Core-Yeni-Baslayanlar/C# Projeleri/Kolay Seviye Projeler/Algoritma/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Algoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BİR METİN VE İNDEX NUMARASI GİRİNİZ:");
+            string text = Console.ReadLine();
+            string word = text.Split(',')[0];
+            int index;
+
+            bool isInt = int.TryParse(text.Split(',')[1],out index);
+
+            if( isInt == true && index < text.Length ) 
+            {
+                List<char> chars = word.ToList();
+                chars.RemoveAt(index);
+
+                foreach( char c in chars ) { Console.Write(c); }
+            }
+            else { Console.WriteLine("indexin uzunluğu ifadenin uzunluğundan fazla olamaz."); }
+            Console.ReadKey();  
+        }
+        
+    }
+}
