491425a3b07246821301d1dd5ce3d4a41d3ce6f7
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-08-11 19:29:20 +0300

    orta seviye algoritma soruları

---
+++ .NET-Core-Yeni-Baslayanlar/C# Projeleri/Orta Seviye Projeler/CharReverse/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boşluk bırakarak kelimeleri giriniz.");
+            string[] str = Console.ReadLine().Split();
+
+            foreach (var item in str)
+            {
+                char[] liste = item.ToCharArray();
+                char tutucu = liste[0];
+                liste[0] = liste[liste.Length - 1];
+                liste[liste.Length - 1] = tutucu;
+
+                string s = "";
+                foreach (var c in liste)
+                    s += c.ToString();
+                Console.Write(s + " ");
+            }
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ .NET-Core-Yeni-Baslayanlar/C# Projeleri/Orta Seviye Projeler/SessizHarf/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SessizHarf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin girin: ");
+            string input = Console.ReadLine();
+
+            string[] kelime = input.Split(' ');
+            string consonants = "bcdfghjklmnpqrstvwxyz";
+
+            foreach (string word in kelime)
+            {
+                bool sessizHarfVarMı = false;
+
+                for (int i = 0; i < word.Length - 1; i++)
+                {
+                    if (consonants.Contains(word[i]) && consonants.Contains(word[i + 1]))
+                    {
+                        sessizHarfVarMı = true;
+                        break;
+                    }
+                }
+
+                Console.Write(sessizHarfVarMı + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
